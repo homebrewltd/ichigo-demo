f8725c44627e8a834faa4fdb2e699e44537394dd
Faisal Amir <urmauur@gmail.com>
2024-10-04 11:20:06 +0700

    feat: add toltip info when user more 3 user

--- components/ui/realtimeTracker.tsx
+++ components/ui/realtimeTracker.tsx
@@ -2,6 +2,14 @@
 
 import { formatCompactNumber } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { FaCircleExclamation } from "react-icons/fa6";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const RealtimeTracker: React.FC = () => {
   const [userCount, setUserCount] = useState<number>(0);
@@ -18,8 +26,26 @@ const RealtimeTracker: React.FC = () => {
     };
   }, []);
 
+  const isShowInfo = userCount > 3;
+
   return (
-    <div>
+    <div className="flex items-center gap-2">
+      <TooltipProvider>
+        {isShowInfo && (
+          <Tooltip>
+            <TooltipTrigger>
+              <FaCircleExclamation className="text-red-400" />
+            </TooltipTrigger>
+            <TooltipContent className="max-w-80 py-2">
+              <p className="text-sm">
+                With more than 3 concurrent users, you may experience increased
+                latency, as this demo runs on a single 3090 GPU
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        )}
+      </TooltipProvider>
+
       <h1>{formatCompactNumber(userCount)} Users Online</h1>
     </div>
   );
